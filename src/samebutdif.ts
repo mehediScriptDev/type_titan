a1c5ddc2e07a57792e9d00e3f6fb0f2c36544c02
mehedi <mehedi25122003@gmail.com>
2026-05-01 18:35:22 +0600

    same same done

--- src/samebutdif.ts
+++ src/samebutdif.ts
@@ -3,3 +3,23 @@ const user = "";
 const test =user ?? "User not found";
 
 console.log(test);
+
+
+
+const discount = (discount: unknown)=>{
+    if(typeof discount === "number"){
+        console.log(discount);
+        
+    }
+    else if(typeof discount === "string"){
+        const perser = discount.split(" ");
+        console.log(Number(perser[0]));
+    }
+    else{
+        console.log("Invalid discount format");
+    }
+}
+
+discount(23);
+discount("11 tk")
+discount(null)
