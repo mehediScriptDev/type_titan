1e0e22384bb48045f6bb41bfbccf619116d44acd
Mehedi <mehedi25122003@gmail.com>
2026-05-11 10:16:56 +0600

    stop tracking node_modules

--- TestModuleApi/src/server.ts
+++ TestModuleApi/src/server.ts
@@ -1,11 +1,11 @@
-import { log } from "console";
 import { createServer, IncomingMessage, Server } from "http";
 
-const server: Server = createServer((req: IncomingMessage,res)=>{
-    console.log(res)
+const server: Server = createServer((req:IncomingMessage,res)=>{
+    console.log("server is ok.");
+    
 })
 
 server.listen(5000,()=>{
-    console.log("server is runnning");
+    console.log("server is running");
     
 })
